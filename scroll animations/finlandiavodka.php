1cb47a675a4dc79c5e06f214879d0ab86a273949
Dereck Smith <derecksmith0204@gmail.com>
2020-11-27 12:26:03 +0530

    creating the https://www.finlandia.com/en/ sun animation

---
+++ scroll animations/finlandiavodka.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+<html xmlns='http://www.w3.org/1999/xhtml'>
+<head>
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<meta name='description' content=''>
+<meta name='keywords' content=''>1`
+<meta name='robots' content='index, follow' />
+<link rel='canonical' href='http://' />
+<meta name='author' content=''>
+<meta name='google-site-verification' content=''>
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
+<link href='https://fonts.googleapis.com/css?family=Hammersmith+One|Lato' rel='stylesheet'>
+<link href='index.css' rel='stylesheet'>
+<title></title>
+</head>
+<body>
+<div class='mainwrapper'>
+    <div class="circle">
+
+    </div>
+</div>
+</body>
+</html>
